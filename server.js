0c45936b12b0c5103738b820f3d98cac7b0e2d47
Arthi135 <arthireddy26@gmail.com>
2026-03-08 12:45:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5174', 'http://localhost:3000'],
+  origin: ['http://localhost:5174', 'http://localhost:3000','https://smartfamer-forntend.vercel.app/'],
   credentials: true
 }));
 app.use(express.json());
